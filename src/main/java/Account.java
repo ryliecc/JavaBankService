f86960f796b15b6088bd9b0d0a8c1d6f7d4b0250
Rylie Castell <rylie.castell@gmail.com>
2023-09-04 15:11:38 +0200

    add bank service

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,7 +1,8 @@
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Account {
-    String accountNumber;
+    String accountId;
     BigDecimal balance;
     Client client;
 
@@ -13,4 +14,46 @@ public class Account {
         balance = balance.subtract(money);
     }
 
+    public Account(String accountNumber, BigDecimal balance, Client client) {
+        this.accountId = accountNumber;
+        this.balance = balance;
+        this.client = client;
+    }
+
+    public String getAccountNumber() {
+        return accountId;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountId = accountNumber;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(accountId, account.accountId) && Objects.equals(balance, account.balance) && Objects.equals(client, account.client);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountId, balance, client);
+    }
 }
---
+++ src/main/java/BankService.java
@@ -0,0 +1,33 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class BankService {
+    List<Account> allAccounts = new ArrayList<>();
+
+    String createNewAccount(Client client){
+        StringBuilder accountId = new StringBuilder("DE");
+        Random rand = new Random();
+        for(int i = 1; i < 21; i++){
+            int random = rand.nextInt(10);
+            accountId.append(random);
+        }
+        String newAccountId = accountId.toString();
+        allAccounts.add(new Account(newAccountId, new BigDecimal("0"), client));
+        return accountId.toString();
+    }
+
+    public BankService(List<Account> allAccounts) {
+        this.allAccounts = allAccounts;
+    }
+
+    public List<Account> getAllAccounts() {
+        return allAccounts;
+    }
+
+    public void setAllAccounts(List<Account> allAccounts) {
+        this.allAccounts = allAccounts;
+    }
+
+}
